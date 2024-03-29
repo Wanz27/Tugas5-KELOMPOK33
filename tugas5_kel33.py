8aff76a53a98d76f58beaeffe6e4ef3a88c8ba63
Attainers <152283978+Fadlyevan@users.noreply.github.com>
2024-03-29 18:26:16 +0700

    Update tugas5_kel33.py

--- tugas5_kel33.py
+++ tugas5_kel33.py
@@ -7,7 +7,17 @@ class Quiz:
             print("Error! Masukkan usia dalam bentuk angka.")
             return
         
-        
+if age >= 18:
+            print(f"Selamat, {name}! Anda bisa ikut kuis.")
+            print("Jawablah pertanyaan berikut ini:")
+            self.ask_question("Berapa hasil dari 12/2?", "6")
+            self.ask_question("Berapa hasil dari 10 + 5?", "15")
+            self.ask_question("Berapa hasil dari 26 - 15?", "11")
+            self.ask_question("Berapa hasil dari 8 x 5?", "40")
+            self.ask_question("Berapa hasil dari 7 x 3?", "21")
+            print(f"Total poin Anda: {self.points}")
+        else:
+            print("Maaf, Anda belum cukup umur untuk ikut kuis.")        
 
 
 
